9c514e5be13799b01df958fa399c39f76920fac6
your name <lathanhmta@gmail.com>
2025-12-23 16:42:37 +0700

    Update stage

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -1,6 +1,13 @@
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "${var.project_name}-gateway"
   protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_headers = ["*"]
+    allow_methods = ["GET", "POST", "OPTIONS"]
+    allow_origins = ["*"]
+    max_age       = 300
+  }
 }
 
 resource "aws_apigatewayv2_stage" "default" {
@@ -21,6 +28,13 @@ resource "aws_apigatewayv2_route" "query_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "POST /ask"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "dev_stage" {
+  api_id = aws_apigatewayv2_api.http_api.id
+  name   = "dev"
+
+  auto_deploy = true
 }
 
 # Cấp quyền cho API Gateway gọi Lambda
