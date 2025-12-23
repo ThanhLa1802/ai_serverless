bf9a5700ae543e6141f1b51647bc2425b460a6c5
your name <lathanhmta@gmail.com>
2025-12-23 16:43:20 +0700

    Update stage

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -10,12 +10,6 @@ resource "aws_apigatewayv2_api" "http_api" {
   }
 }
 
-resource "aws_apigatewayv2_stage" "default" {
-  api_id      = aws_apigatewayv2_api.http_api.id
-  name        = "$default"
-  auto_deploy = true
-}
-
 # Kết nối API Gateway tới Lambda
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = aws_apigatewayv2_api.http_api.id
